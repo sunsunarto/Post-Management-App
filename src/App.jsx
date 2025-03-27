eccd9b0b833da26015d2f11d2de0b6e94e323790
sunsunarto <sunaryo24259@outlook.com>
2025-03-27 11:13:14 +0700

    fouth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState,useEffect } from 'react'
-import './App.css'
-import PostList from './Components/PostList.jsx'
-import axios from 'axios'
-import PostForm from './Components/PostForm.jsx'
+import { useState, useEffect } from 'react';
+import './App.css';
+import PostList from './Components/PostList.jsx';
+import axios from 'axios';
+import PostForm from './Components/PostForm.jsx';
+
 function App() {
-  const [posts,setPost]= useState([])
-    useEffect (() => {
-      const fetchPosts = async () => {
-      try{
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPost(response.data)
-      }catch(error){
-        console.error('Error fetching posts:',error)
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
       }
-      }  
-      fetchPosts()
-    },[])
-    const addPost = (newPost) => {
-      setPost([newPost,...posts ]);
+    };
+    fetchPosts();
+  }, []);
+
+  const addPost = (newPost) => {
+    setPosts([newPost, ...posts]);
   };
 
   return (
-      <div className='body'>
-          <h1>Post Management App</h1>
-          <PostForm addPost={addPost} />
-          <h1>Recent Post</h1>
-          <PostList posts={posts} />
-      </div>
+    <div className='body'>
+      <h1>Post Management App</h1>
+      <PostForm addPost={addPost} />
+      <h1>Recent Posts</h1>
+      <PostList posts={posts} />
+    </div>
   );
-};
+}
 
+export default App;
 
-export default App
